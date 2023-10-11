92a26613b84d7bf3a859cfe985d7cde42577a328
Takashi Mori <kanetugu2015@gmail.com>
2023-10-12 07:57:40 +0900

    bug fixed

--- src/px4sim_main.cpp
+++ src/px4sim_main.cpp
@@ -1,40 +1,49 @@
-
 #include "comm/udp_connector.hpp"
 #include <iostream>
 #include <cstdlib> // for std::atoi
 
 int main(int argc, char* argv[]) 
 {
-    if(argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port>" << std::endl;
+    if(argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " <remote_ip> <remote_port> <server_ip> <server_port>" << std::endl;
         return -1;
     }
 
-    const char* serverIp = argv[1];
-    int serverPort = std::atoi(argv[2]);
+    const char* remoteIp = argv[1];
+    int remotePort = std::atoi(argv[2]);
+    const char* serverIp = argv[3];
+    int serverPort = std::atoi(argv[4]);
 
-    // 自動採番されるポート番号を使う場合、0を指定します。
-    //hako::px4::comm::IcommEndpointType localEndpoint = { nullptr, 0 };
+    hako::px4::comm::IcommEndpointType remoteEndpoint = { remoteIp, remotePort };
     hako::px4::comm::IcommEndpointType serverEndpoint = { serverIp, serverPort };
 
-    hako::px4::comm::UdpConnector udpConnector;
+    hako::px4::comm::UdpConnector clientConnector;
 
-    if (!udpConnector.server_open(&serverEndpoint)) 
+    if (!clientConnector.client_open(&serverEndpoint, &remoteEndpoint)) 
     {
-        std::cerr << "Failed to open UDP server" << std::endl;
+        std::cerr << "Failed to open UDP client" << std::endl;
         return -1;
+    }
+
+    // Example: Sending data
+    const char* sendData = "Hello from PX4 Sim!";
+    int sentDataLen;
+    if(clientConnector.send(sendData, strlen(sendData) + 1, &sentDataLen)) { // +1 to include null terminator
+        std::cout << "Sent data: " << sendData << " with length: " << sentDataLen << std::endl;
+    } else {
+        std::cerr << "Failed to send data" << std::endl;
     }
 
     // Example: Receiving data
     char recvBuffer[1024];
     int recvDataLen;
-    if (udpConnector.recv(recvBuffer, sizeof(recvBuffer), &recvDataLen)) 
+    if (clientConnector.recv(recvBuffer, sizeof(recvBuffer), &recvDataLen)) 
     {
         std::cout << "Received data: " << recvBuffer << " with length: " << recvDataLen << std::endl;
     } else {
         std::cerr << "Failed to receive data" << std::endl;
     }
 
-    udpConnector.close();
+    clientConnector.close();
     return 0;
 }
