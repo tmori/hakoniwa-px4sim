74e14296d5a86507fb1d88c8b8b3d3bdf1c4506b
Takashi Mori <kanetugu2015@gmail.com>
2023-10-21 14:51:04 +0900

    add asset_time

--- src/hako/runner/hako_px4_runner.cpp
+++ src/hako/runner/hako_px4_runner.cpp
@@ -4,7 +4,11 @@
 #include "../pdu/hako_pdu_data.hpp"
 #include <iostream>
 
-HakoPx4RunnerArgType *px4_arg = nullptr; 
+typedef struct {
+    HakoPx4RunnerArgType *arg;
+    uint64_t asset_time;
+} HakoPx4ControlType;
+HakoPx4ControlType hako_px4_control;
 
 static void my_setup()
 {
@@ -15,7 +19,7 @@ static void my_task()
     // READ SHARING ACTUATOR AND WRITE PDU ACTUATOR
     Hako_HakoHilActuatorControls hil_actuator_controls;
     if (hako_read_hil_actuator_controls(hil_actuator_controls)) {
-        if (!hako_asset_runner_pdu_write(px4_arg->robo_name, 
+        if (!hako_asset_runner_pdu_write(hako_px4_control.arg->robo_name, 
                 HAKO_PX4_CHANNLE_ID_HIL_ACTUATOR_CONTROLS, 
                 (const char*)&hil_actuator_controls, 
                 sizeof(hil_actuator_controls))) {
@@ -26,13 +30,13 @@ static void my_task()
     Hako_HakoHilSensor hil_sensor;
     Hako_HakoHilGps hil_gps;
     Hako_HakoHilStateQuaternion hil_state_quaternion;
-    if (hako_asset_runner_pdu_read(px4_arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_SENSOR, (char*)&hil_sensor, sizeof(hil_sensor))) {
+    if (hako_asset_runner_pdu_read(hako_px4_control.arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_SENSOR, (char*)&hil_sensor, sizeof(hil_sensor))) {
         hako_write_hil_sensor(hil_sensor);
     }
-    if (hako_asset_runner_pdu_read(px4_arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_GPS, (char*)&hil_gps, sizeof(hil_gps))) {
+    if (hako_asset_runner_pdu_read(hako_px4_control.arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_GPS, (char*)&hil_gps, sizeof(hil_gps))) {
         hako_write_hil_gps(hil_gps);
     }
-    if (hako_asset_runner_pdu_read(px4_arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_STATE_QUATERNION, (char*)&hil_state_quaternion, sizeof(hil_state_quaternion))) {
+    if (hako_asset_runner_pdu_read(hako_px4_control.arg->robo_name, HAKO_PX4_CHANNLE_ID_HIL_STATE_QUATERNION, (char*)&hil_state_quaternion, sizeof(hil_state_quaternion))) {
         hako_write_hil_state_quaternion(hil_state_quaternion);
     }
 }
@@ -50,18 +54,22 @@ static hako_asset_runner_callback_t my_callbacks = {
 
 void *hako_px4_runner(void *argp)
 {
-    px4_arg = static_cast<HakoPx4RunnerArgType*>(argp);
-    if (hako_asset_runner_init(px4_arg->asset_name, px4_arg->config_path, px4_arg->delta_time_msec) == false) {
+    hako_px4_control.arg = static_cast<HakoPx4RunnerArgType*>(argp);
+    if (hako_asset_runner_init(hako_px4_control.arg->asset_name, hako_px4_control.arg->config_path, hako_px4_control.arg->delta_time_msec) == false) {
         std::cerr << "ERROR: " << "hako_asset_runner_init() error" << std::endl;
         return nullptr;
     }
     hako_asset_runner_register_callback(&my_callbacks);
     while (true) {
+        hako_px4_control.asset_time = 0;
         std::cout << "INFO: start simulation" << std::endl;
         while (true) {
             if (hako_asset_runner_step(1) == false) {
                 std::cout << "INFO: stopped simulation" << std::endl;
                 break;
+            }
+            else {
+                hako_px4_control.asset_time++;
             }
             //std::cout << "STEP" << std::endl;
         }
