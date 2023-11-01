3824025823a573935849ccb5919ace23c931592f
Takashi Mori <kanetugu2015@gmail.com>
2023-11-01 15:39:06 +0900

    add common funcs

--- src/hako/runner/drone/drone_phys.hpp
+++ src/hako/runner/drone/drone_phys.hpp
@@ -1,12 +1,57 @@
 #ifndef _DRONE_PHYS_HPP_
 #define _DRONE_PHYS_HPP_
 
+#include "../../pdu/hako_pdu_data.hpp"
+
+typedef struct {
+    uint64 up_cycle_hil_sensor;
+    uint64 up_cycle_hil_gps;
+    uint64 up_cycle_hil_state_quaternion;
+    Hako_HakoHilSensor hil_sensor;
+    Hako_HakoHilGps hil_gps;
+    Hako_HakoHilStateQuaternion hil_state_quaternion;
+} DroneSensorType;
+
+
 typedef struct {
     double x;
     double y;
     double z;
 } Vector3Type;
+typedef struct {
+    double w;
+    double x;
+    double y;
+    double z;
+} QuaternionType;
+static inline void euler2Quaternion(const Vector3Type& e, QuaternionType& ret)
+{
+    // Compute the half angles
+    double cy = cos(e.z * 0.5); // yaw
+    double sy = sin(e.z * 0.5);
+    double cp = cos(e.y * 0.5); // pitch
+    double sp = sin(e.y * 0.5);
+    double cr = cos(e.x * 0.5); // roll
+    double sr = sin(e.x * 0.5);
 
+    ret.w = cr * cp * cy + sr * sp * sy;
+    ret.x = sr * cp * cy - cr * sp * sy;
+    ret.y = cr * sp * cy + sr * cp * sy;
+    ret.z = cr * cp * sy - sr * sp * cy;
+}
+
+static inline void vector3_plus(const Vector3Type& l, const Vector3Type& r, Vector3Type& ret)
+{
+    ret.x = l.x + r.x;
+    ret.y = l.y + r.y;
+    ret.z = l.z + r.z;
+}
+static inline void vector3_minus(const Vector3Type& l, const Vector3Type& r, Vector3Type& ret)
+{
+    ret.x = l.x - r.x;
+    ret.y = l.y - r.y;
+    ret.z = l.z - r.z;
+}
 #define DRONE_PROPELLER_NUM 4
 
 typedef struct {
@@ -41,6 +86,68 @@ typedef struct {
     Vector3Type rot_vec;
 } DronePhysStateType;
 
+#define SENSOR_MAX_COUNT_NUM    10
+typedef struct {
+    int inx;
+    int max_count;
+    int data_num;
+    Vector3Type values[SENSOR_MAX_COUNT_NUM];
+} DroneSensorAverageDataType;
+static inline bool initAverageData(DroneSensorAverageDataType& data, int max_count)
+{
+    if (max_count > SENSOR_MAX_COUNT_NUM || max_count <= 0) {
+        return false;  // 入力されたmax_countが範囲外であれば、失敗を返す
+    }
+
+    data.inx = 0;
+    data.max_count = max_count;
+    data.data_num = 0;
+    for (int i = 0; i < SENSOR_MAX_COUNT_NUM; ++i) {
+        data.values[i].x = 0.0;
+        data.values[i].y = 0.0;
+        data.values[i].z = 0.0;
+    }
+
+    return true;  // 初期化成功
+}
+static inline void addAverageData(DroneSensorAverageDataType& data, const Vector3Type value)
+{
+    // Add the new value to the values array at the current index
+    data.values[data.inx] = value;
+
+    // Update the index for the next data point
+    data.inx = (data.inx + 1) % data.max_count;
+
+    // Update the data count (but don't let it exceed SENSOR_AVERAGE_COUNT)
+    if (data.data_num < data.max_count)
+    {
+        data.data_num++;
+    }
+}
+
+static inline void calcAverage(DroneSensorAverageDataType& data, Vector3Type& result)
+{
+    result.x = 0;
+    result.y = 0;
+    result.z = 0;
+
+    // Sum up all the values in the array
+    for (int i = 0; i < data.data_num; i++)
+    {
+        result.x += data.values[i].x;
+        result.y += data.values[i].y;
+        result.z += data.values[i].z;
+    }
+
+    // Calculate the average
+    if (data.data_num > 0)
+    {
+        result.x /= data.data_num;
+        result.y /= data.data_num;
+        result.z /= data.data_num;
+    }
+}
+
 typedef struct {
     /*
      * constant values
@@ -54,6 +161,11 @@ typedef struct {
     double current_time;
     DronePhysStateType current;
     DronePhysStateType next;
+    /*
+     * Sensors
+     */
+    DroneSensorType sensor;
+    DroneSensorAverageDataType sensor_acc;
 } DronePhysType;
 
 
