f120c031f8506b17e59605d23468b2304a5752fe
Takashi Mori <kanetugu2015@gmail.com>
2023-11-03 08:47:13 +0900

    add y axis control

--- src/hako/runner/control/drone_control.cpp
+++ src/hako/runner/control/drone_control.cpp
@@ -12,10 +12,14 @@ constexpr float DRONE_CONTROL_PID_ROT_X_KP = 20.0f;  // 比例ゲイン
 constexpr float DRONE_CONTROL_PID_ROT_X_KI = 1.0f;  // 積分ゲイン
 constexpr float DRONE_CONTROL_PID_ROT_X_KD = 1.0f;  // 微分ゲイン
 
+constexpr float DRONE_CONTROL_PID_ROT_Y_KP = 20.0f;  // 比例ゲイン
+constexpr float DRONE_CONTROL_PID_ROT_Y_KI = 1.0f;  // 積分ゲイン
+constexpr float DRONE_CONTROL_PID_ROT_Y_KD = 1.0f;  // 微分ゲイン
 
 constexpr float DRONE_THRUST_MAX = 20.0f;
 constexpr float DRONE_THRUST_MIN = 0.0f;
 constexpr float DRONE_TORQUE_X_MAX_RATE = 1.0f;
+constexpr float DRONE_TORQUE_Y_MAX_RATE = 1.0f;
 
 static inline double get_value_with_limit(double value, double max, double min)
 {
@@ -57,12 +61,15 @@ void drone_control_init(DroneControlType& ctrl, double delta_t)
     ctrl.delta_t = delta_t;
     ctrl.target_pos.target.z = 6000.0f;   // 単位：mm
 
-    ctrl.target_rot.target.x = 0;
     // Z軸用PIDコントローラの初期化
     initPID(ctrl.target_pos.pid_z, DRONE_CONTROL_PID_POS_Z_KP, DRONE_CONTROL_PID_POS_Z_KI, DRONE_CONTROL_PID_POS_Z_KD, ctrl.target_pos.target.z);
 
     //X軸の水平制御
+    ctrl.target_rot.target.x = 0;
     initPID(ctrl.target_rot.pid_roll, DRONE_CONTROL_PID_ROT_X_KP, DRONE_CONTROL_PID_ROT_X_KI, DRONE_CONTROL_PID_ROT_X_KD, ctrl.target_rot.target.x);
+    //Y軸の水平制御
+    ctrl.target_rot.target.y = 0;
+    initPID(ctrl.target_rot.pid_pitch, DRONE_CONTROL_PID_ROT_Y_KP, DRONE_CONTROL_PID_ROT_Y_KI, DRONE_CONTROL_PID_ROT_Y_KD, ctrl.target_rot.target.y);
 
     // initPID関数がPIDControllerのインスタンスを適切に初期化することを想定しています。
 }
@@ -91,27 +98,44 @@ void drone_control_run(DroneControlType& ctrl)
     std::cout << "hil_state_quaternion.lon= " << hil_state_quaternion.lon << std::endl;
 
     // roll power
-    static double roll_power_for_move = 0;
+    static Vector3Type roll_power_for_move = { 0, 0, 0 };
     std::cout << "ctrl.target_rot.pid_roll.previous_error= " << ctrl.target_rot.pid_roll.previous_error << std::endl;
     int c = getc(stdin);
     if (c == 'j') {
         std::cout << "key=" << c << std::endl;
-        roll_power_for_move = -1.0;
-    }
-    else if (c == ' ') {
-        std::cout << "key=" << c << std::endl;
-        roll_power_for_move = 0;
+        roll_power_for_move.x = -1.0;
     }
     else if (c == 'l') {
         std::cout << "key=" << c << std::endl;
-        roll_power_for_move = 1.0;
+        roll_power_for_move.x= 1.0;
     }
-    double roll_power_for_same = updatePID(ctrl.target_rot.pid_roll, angle.x, ctrl.delta_t);    
+    else if (c == 'i') {
+        std::cout << "key=" << c << std::endl;
+        roll_power_for_move.y= 1.0;
+    }
+    else if (c == 'm') {
+        std::cout << "key=" << c << std::endl;
+        roll_power_for_move.y= -1.0;
+    }
+    else if (c == ' ') {
+        std::cout << "key=" << c << std::endl;
+        roll_power_for_move.x = 0;
+        roll_power_for_move.y = 0;
+    }
+    Vector3Type roll_power_for_same;
+    roll_power_for_same.x = updatePID(ctrl.target_rot.pid_roll, angle.x, ctrl.delta_t);    
+    roll_power_for_same.y = updatePID(ctrl.target_rot.pid_pitch, angle.y, ctrl.delta_t);    
 
-    std::cout << "roll_power_for_move= " << roll_power_for_move << std::endl;
-    std::cout << "roll_power_for_same= " << roll_power_for_same << std::endl;
-    double torque_x = roll_power_for_move + roll_power_for_same;
+    std::cout << "roll_power_for_move.x= " << roll_power_for_move.x << std::endl;
+    std::cout << "roll_power_for_same.x= " << roll_power_for_same.x << std::endl;
+    std::cout << "roll_power_for_move.y= " << roll_power_for_move.y << std::endl;
+    std::cout << "roll_power_for_same.y= " << roll_power_for_same.y << std::endl;
+    double torque_x = roll_power_for_move.x + roll_power_for_same.x;
     double torque_x_max = ctrl.signal.thrust * DRONE_PARAM_L / (2.0 * DRONE_TORQUE_X_MAX_RATE);
     ctrl.signal.torque.x = get_value_with_limit(torque_x, torque_x_max, -torque_x_max);
+
+    double torque_y = roll_power_for_move.y + roll_power_for_same.y;
+    double torque_y_max = ctrl.signal.thrust * DRONE_PARAM_L / (2.0 * DRONE_TORQUE_Y_MAX_RATE);
+    ctrl.signal.torque.y = get_value_with_limit(torque_y, torque_y_max, -torque_y_max);
 
 }
